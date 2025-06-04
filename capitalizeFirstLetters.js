e5839f21296bb02b5117a8df154622cea6293e08
bagzi40 <jbagumaa@gmail.com>
2025-06-04 17:47:00 +0300

    Updated capitalizeFirstLetters.js

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,10 +1,9 @@
 let sentence = "hello world";
 // use spiit to seperate words
 let words = sentence.split(" "); // splits words which isi hello, and world
-let capitalisedWords = words.map(function(word) {
-// makes first letter capital
-return word[0]. toUpperCase() + word.slice(1);
-});
-// join the words back together agian
-let result = capitalisedWords.join(" ");
+let result = "";
+for (let word of words){
+    result += word[0].toUpperCase() + word.slice(1) + " "; 
+
+}
 console.log(result);
